343ac85870c72240d30ada05685fee66e9c58f20
SusaninaJulia <jsusanina@gmail.com>
2020-03-13 18:14:10 +0300

    Add files via upload

--- PBMM-gpu/modification/main-substrings.cpp
+++ PBMM-gpu/modification/main-substrings.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <sstream> 
 #include <chrono>
 #include "ParsingTable.h"
 
@@ -7,12 +7,19 @@ int main(int argc, char *argv[]) {
 
     ParsingTable table = ParsingTable(argv[1]);
     Graph graph = Graph(argv[2]);
+    int s = 0;
+    std::stringstream sublen(argv[3]);
+    sublen >> s;
 
     auto begin = std::chrono::high_resolution_clock::now();
     std::cout << "modification:" << std::endl;
     
-    auto times = table.modification(graph);
-    std::cout << times << std::endl;
+    std::vector<std::pair<int, int>> res = table.modification_for_substrings(graph, s);
+    std::cout << res.size() << std::endl;
+//    for(int i = 0; i < res.size(); i++)
+//    {
+//         std::cout << res[i].first << ", " << res[i].second << std::endl;
+//    }
 
     auto end = std::chrono::high_resolution_clock::now();
     auto dur = end - begin;
